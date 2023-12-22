8fabf3035b689f9a6c168bfc9b22a9cbbbc4dae8
Павел Сарыгин <paulssssar@gmail.com>
2023-12-22 11:59:39 +0700

    удалил .idea

--- short_tracker_bot/handlers/hello.py
+++ short_tracker_bot/handlers/hello.py
@@ -1,10 +1,12 @@
 from aiogram import F, Router
 from aiogram import types
+from short_tracker.users.models import CustomUser
+from django.shortcuts import
 
 router = Router()
 
 
 @router.message(F.text == '/start')
 async def hello(message: types.Message):
-    await message.answer('''Здравствуйте! Я бот трэкера задач 
-    https://short-tracker.acceleratorpracticum.ru. Чем я могу Вам помочь?''')
+    await message.answer('''Здравствуйте! Я бот трэкера задач
+    #https://short-tracker.acceleratorpracticum.ru. Чем я могу Вам помочь?''')
