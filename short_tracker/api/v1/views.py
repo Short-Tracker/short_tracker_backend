baa10f4eb892574b98a8e562ccfa1148405fb303
Timur <belikov.t9@yandex.ru>
2023-12-28 17:56:35 +0300

    JWT in Cookie

--- short_tracker/api/v1/views.py
+++ short_tracker/api/v1/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
- 
+
 from .filters import TaskFilter
 from .serializers import (
     AuthSignInSerializer,
