b392a38611f22f96361beae959877cb660f2e44f
Timur <belikov.t9@yandex.ru>
2023-12-28 17:58:23 +0300

    JWT in Cookie

--- short_tracker/users/authentication.py
+++ short_tracker/users/authentication.py
@@ -5,20 +5,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class CookieJWTAuthentication(JWTAuthentication):
 
     def authenticate(self, request: Request) -> tuple:
-        """
-        Authenticates a request by extracting the token from the
-        header or cookie, validating it, and returning the user
-        associated with the token along with the validated token.
-
-        Parameters:
-            request (object): The request object containing the header
-            and cookie information.
-
-        Returns:
-            tuple: A tuple containing the user associated with the validated
-            token and the validated token itself. If the request does not
-            contain a valid token, None is returned.
-        """
         header = self.get_header(request)
 
         if header is not None:
@@ -34,13 +20,4 @@ class CookieJWTAuthentication(JWTAuthentication):
         return self.get_user(validated_token), validated_token
 
     def get_raw_token_cookie(self, request: Request) -> str:
-        """
-        Retrieve the raw JWT token from the request.
-
-        Args:
-            request (HttpRequest): The HTTP request object.
-
-        Returns:
-            str: The raw JWT token from the request cookies.
-        """
         return request.COOKIES.get('jwt')
