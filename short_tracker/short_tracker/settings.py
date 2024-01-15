2ce91bf8cc386cb4389da79e72c78dc4c21ffd63
Воложанин Пётр <mister.volojanin@yandex.ru>
2024-01-15 22:25:56 +0700

    small fix

--- short_tracker/short_tracker/settings.py
+++ short_tracker/short_tracker/settings.py
@@ -137,7 +137,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'users.authentication.CookieJWTAuthentication',
