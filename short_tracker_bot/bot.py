77a039856708e8d709d959f1954a6a6373a038f0
Павел Сарыгин <paulssssar@gmail.com>
2024-01-17 18:55:00 +0700

    исправил импорт

--- short_tracker_bot/bot.py
+++ short_tracker_bot/bot.py
@@ -19,9 +19,3 @@ async def main():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
-
-
-
-
-
-
--- short_tracker_bot/handlers/hello.py
+++ short_tracker_bot/handlers/hello.py
@@ -1,10 +1,20 @@
-from aiogram import F, Router
+import datetime
+
+from aiogram import F, Router, Bot
 from aiogram import types
 from aiogram.filters import CommandStart
 from keyboards.keyboards import start_keyboard
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+import logging
 import requests
+import time
+from short_tracker_bot.config import URL
+
+URLS = {
+
+}
+
 router = Router()
 
 
@@ -12,11 +22,48 @@ class Login(StatesGroup):
     email = State()
     password = State()
     new_token = State()
+    response = State()
+
+
+async def get_status(state: FSMContext, bot: Bot):
+    data_fsm = await state.get_data()
+    token = data_fsm['new_token']
+    headers = {
+        'Authorization': token
+    }
+    tasks = dict()
+    tasks_expired = []
+    while True:
+        try:
+            messages = requests.get(
+                URL + 'tasks/',
+                headers=headers).json()
+            chat_id = data_fsm['chat_id']
+            for msg in messages['results']:
+                deadline = datetime.datetime.strptime(msg['deadline_date'], '%Y-%m-%d').date()
+                if msg['description'] not in tasks.keys():
+                    tasks[msg['description']] = msg['status']
+                if msg['status'] != tasks[msg['description']]:
+                    tasks[msg['description']] = msg['status']
+                    await bot.send_message(
+                        chat_id=chat_id,
+                        text=f'Изменен статус задачи {msg["description"]} на {msg["status"]}')
+                if datetime.date.today() > deadline and msg['description'] not in tasks_expired:
+                    tasks_expired.append(msg['description'])
+                    performers = [performer['full_name'] for performer in msg['performers']]
+                    await bot.send_message(
+                        chat_id=chat_id,
+                        text=f'Задача {msg["description"]} сотрудника'
+                             f' {", ".join(performers)} просрочена'
+                    )
+        except Exception:
+            await bot.send_message(data_fsm['chat_id'], 'Произошла ошибка')
+        finally:
+            time.sleep(15)
 
 
 @router.message(CommandStart())
 async def hello(message: types.Message):
-    messages = requests.get('http://127.0.0.1:8000/api/v1/messages/').json()
     await message.answer('''Здравствуйте! Я бот трэкера задач
     #https://short-tracker.acceleratorpracticum.ru. Чем я могу Вам помочь?''',
                          reply_markup=start_keyboard)
@@ -24,6 +71,7 @@ async def hello(message: types.Message):
 
 @router.message(F.text == '/войти')
 async def email(message: types.Message, state: FSMContext):
+    await state.update_data(chat_id=message.chat.id)
     await state.set_state(Login.email)
     await message.answer('Введите свою почту')
 
@@ -36,32 +84,24 @@ async def password(message: types.Message, state: FSMContext):
 
 
 @router.message(Login.password)
-async def get_token(message: types.Message, state: FSMContext):
+async def get_token(message: types.Message, state: FSMContext, bot: Bot):
     await state.update_data(password=message.text)
     data_fsm = await state.get_data()
     data = {
         'email': data_fsm['email'],
         'password': data_fsm['password']
     }
-    get_token = requests.post(
-        'http://127.0.0.1:8000/api/v1/auth/login/',
-        data=data
-    )
-    token = get_token.cookies.get('jwt_access')
-    await state.update_data(new_token=f'Bearer {token}')
-    data_fsm = await state.get_data()
-    token = data_fsm['new_token']
-    headers = {
-        'Authorization': token
-    }
-    # messages = requests.get(
-    #     'http://127.0.0.1:8000/api/v1/messages/',
-    #     headers=headers
-    # ).json()
-    # for msg in messages['results']:
-    #     await message.answer(msg['message_body'])
-
-
-
-
-
+    try:
+        get_token = requests.post(
+            URL + 'auth/login/',
+            data=data
+        )
+        token = get_token.cookies.get('jwt_access')
+        if token:
+            await state.update_data(new_token=f'Bearer {token}')
+            await state.set_state(Login.response)
+            await get_status(state, bot)
+        else:
+            await message.answer(text='Неверный логин или пароль')
+    except Exception:
+        await bot.send_message(data_fsm['chat_id'], 'Нет соединения')
