ab39b7a13c9f6a38a5a829cb58d9d4d0e3d8749e
Bair Erendzhenov <erendzhenovbair1990@yandex.ru>
2024-01-22 00:37:36 +0300

    Изменил подсчет аналитики

--- short_tracker/api/v1/analytics.py
+++ short_tracker/api/v1/analytics.py
@@ -8,58 +8,58 @@ class TasksAnalyticsFactory:
     def calculate_analytics(queryset):
         analytics = {}
         total_tasks = queryset.count()
-        completed_on_time = queryset.filter(
-            status=Task.TaskStatus.DONE,
-            finish_date__lte=F('deadline_date')
-        ).count()
-        analytics['completed_on_time_count'] = completed_on_time
-        analytics['completed_on_time_percentage'] = (
-            (completed_on_time / total_tasks) * 100
-        ) if total_tasks > 0 else 0
-        completed_with_delay = queryset.filter(
-            status=Task.TaskStatus.DONE,
-            finish_date__gt=F('deadline_date')
-        ).count()
-        analytics['completed_with_delay_count'] = completed_with_delay
-        analytics['completed_with_delay_percentage'] = (
-            (completed_with_delay / total_tasks) * 100
-        ) if total_tasks > 0 else 0
+        analytics.update({
+            'completed_on_time_count': queryset.filter(
+                status=Task.TaskStatus.DONE,
+                finish_date__lte=F('deadline_date')
+            ).count(),
+            'completed_with_delay_count': queryset.filter(
+                status=Task.TaskStatus.DONE,
+                finish_date__gt=F('deadline_date')
+            ).count(),
+        })
 
-        average_time_todo_to_inprogress = queryset.aggregate(
-            avg_time=ExpressionWrapper(
-                Sum(F('inprogress_date') - F('start_date')),
-                output_field=fields.DurationField()
-            )
-        )['avg_time_todo_to_inprogress']
-        analytics['average_time_todo_to_inprogress'] = (
-            average_time_todo_to_inprogress / total_tasks
-        ) if total_tasks > 0 else 0
+        analytics.update(
+            TasksAnalyticsFactory._calculate_percentage(analytics, total_tasks)
+        )
 
-        average_time_todo_to_done = queryset.filter(
-            status=Task.TaskStatus.DONE,
-            inprogress_date__isnull=False,
-            start_date__isnull=False
-        ).aggregate(
-            avg_time=ExpressionWrapper(
-                Sum(F('finish_date') - F('start_date')),
-                output_field=fields.DurationField()
+        analytics.update(
+            TasksAnalyticsFactory._calculate_average_time(
+                queryset, total_tasks
             )
-        )['avg_time_todo_to_done']
-        analytics['average_time_todo_to_done'] = (
-            average_time_todo_to_done / total_tasks
-        ) if total_tasks > 0 else 0
+        )
 
-        average_time_inprogress_to_done = queryset.filter(
-            status=Task.TaskStatus.DONE,
-            inprogress_date__isnull=False
-        ).aggregate(
-            avg_time=ExpressionWrapper(
-                Sum(F('finish_date') - F('inprogress_date')),
-                output_field=fields.DurationField()
-            )
-        )['avg_time_inprogress_to_done']
-        analytics['average_time_inprogress_to_done'] = (
-            average_time_inprogress_to_done / total_tasks
-        )if total_tasks > 0 else 0
-        
         return analytics
+
+    @staticmethod
+    def _calculate_percentage(analytics, total_tasks):
+        return {
+            f'{key}_percentage': (
+                (value / total_tasks) * 100
+            ) if total_tasks > 0 else 0
+            for key, value in analytics.items()
+        }
+
+    @staticmethod
+    def _calculate_average_times(queryset, total_tasks):
+        def calculate_avg_time(field1, field2):
+            avg_time_key = f'average_time_{field1}_to_{field2}'
+            avg_time = queryset.filter(
+                status=Task.TaskStatus.DONE,
+                **{f'{field1}__isnull': False, f'{field2}__isnull': False}
+            ).aggregate(
+                avg_time=ExpressionWrapper(
+                    Sum(F(field2) - F(field1)),
+                    output_field=fields.DurationField()
+                )
+            )['avg_time']
+
+            return {
+                avg_time_key: avg_time / total_tasks if total_tasks > 0 else 0
+            }
+
+        return {
+            **calculate_avg_time('start_date', 'inprogress_date'),
+            **calculate_avg_time('start_date', 'finish_date'),
+            **calculate_avg_time('inprogress_date', 'finish_date'),
+        }
