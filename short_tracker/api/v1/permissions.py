a713eb4585cd6adcc5ac6cdad51e82b6a4196f1b
Bair Erendzhenov <erendzhenovbair1990@yandex.ru>
2024-01-23 17:13:45 +0300

    Подправил IsTeamLead

--- short_tracker/api/v1/permissions.py
+++ short_tracker/api/v1/permissions.py
@@ -3,6 +3,4 @@ from rest_framework import permissions
 
 class IsTeamLead(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated and request.user.is_team_lead:
-            return True
-        return False
+         return request.user.is_authenticated and request.user.is_team_lead
