8efb41180a40358b23648a1ff18cecb72d09cac4
Bair Erendzhenov <erendzhenovbair1990@yandex.ru>
2024-01-23 15:58:50 +0300

    добавил валидацию

--- short_tracker/api/v1/analytics/serializers.py
+++ short_tracker/api/v1/analytics/serializers.py
@@ -19,3 +19,24 @@ class TaskAnalyticsSerializer(TaskSerializer):
             'average_time_todo_to_inprogress', 'average_time_todo_to_done',
             'average_time_inprogress_to_done',
         )
+    
+    def validate(self, data):
+        validated_data = super().validate(data)
+        total_percentage = (
+            validated_data['completed_on_time_percentage'] +
+            validated_data['completed_with_delay_percentage']
+        )
+        if total_percentage != 100:
+            raise serializers.ValidationError(
+                "Сумма процентов должна быть равна 100%."
+            )
+        for field in [
+            'average_time_todo_to_inprogress',
+            'average_time_todo_to_done',
+            'average_time_inprogress_to_done',
+        ]:
+            if validated_data[field] < 0:
+                raise serializers.ValidationError(
+                    f"{field} не может быть отрицательным."
+                )
+        return validated_data
