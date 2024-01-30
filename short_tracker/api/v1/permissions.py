a936dbefb710d102143ad86a3c4df1a04d998326
Timur <belikov.t9@yandex.ru>
2024-01-30 20:50:34 +0300

    fixed

--- short_tracker/api/v1/permissions.py
+++ short_tracker/api/v1/permissions.py
@@ -6,7 +6,7 @@ class IsTeamLead(permissions.BasePermission):
         return request.user.is_authenticated and request.user.is_team_lead
 
 
-class isLeadOrPerformerHimselfOnly(permissions.BasePermission):
+class IsLeadOrPerformerHimselfOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         is_lead = request.user.is_lead
